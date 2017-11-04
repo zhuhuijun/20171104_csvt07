6a8d33ba04046c36b1bdcc2b6da9df554e7dfb29
kngcbzdsn <zhuhjtcx@qq.com>
2017-11-04 16:58:53 +0800

    gh

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,9 @@
 # Create your views here.
 # coding:utf-8 	
 from blog.models import Author,Book
-from django.shortcuts import render_to_responde
+from django.shortcuts import render_to_response
 
 
 def show_author(req):
 	authors = Author.objects.all()
-	return render_to_responde('show_author.html',{'authors':authors})
+	return render_to_response('show_author.html',{'authors':authors})
