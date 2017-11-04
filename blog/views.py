9f85f5e7b58f3c7e111cbf1cf1a2c9bbd6c4df07
kngcbzdsn <zhuhjtcx@qq.com>
2017-11-04 16:38:53 +0800

    index

--- blog/views.py
+++ blog/views.py
@@ -1,1 +1,9 @@
 # Create your views here.
+# coding:utf-8 	
+from blog.models import Author,Book
+from django.shortcuts import render_to_responde
+
+
+def show_author(req):
+	authors = Author.objects.all()
+	return render_to_responde('show_author.html',{'authors':authors})
