7f29042145c75179130d90cc628f1b415e159b4e
kngcbzdsn <zhuhjtcx@qq.com>
2017-11-04 16:55:05 +0800

    rt

--- urls.py
+++ urls.py
@@ -13,6 +13,6 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
     url(r'^/blog/show_author/$','blog.views.show_author'),
 )
