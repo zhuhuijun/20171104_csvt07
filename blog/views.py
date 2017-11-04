e5a11285dc0169ad59755f377b29615b47f46a0a
kngcbzdsn <zhuhjtcx@qq.com>
2017-11-04 17:11:51 +0800

    gg

--- blog/views.py
+++ blog/views.py
@@ -7,3 +7,7 @@ from django.shortcuts import render_to_response
 def show_author(req):
 	authors = Author.objects.all()
 	return render_to_response('show_author.html',{'authors':authors})
+
+def show_book(req):
+	books = Book.objects.all()
+	return render_to_response('show_book.html',{'books',books})
